c1d60738839544eaeba0d2438a3282100d6e2fb3
Jonathan Lehman <jonathan.lehman91@gmail.com>
2016-05-25 16:38:32 -0400

    Fix parsing error

--- base.js
+++ base.js
@@ -26,7 +26,7 @@ module.exports = {
     "generator-star-spacing": [2, { "before": true, "after": true }],
     "handle-callback-err": [2, "^(err|error)$" ],
     "indent": [2, 2, { "SwitchCase": 1 }],
-    "key-spacing": [2, { "mode": "minimum" }]
+    "key-spacing": [2, { "mode": "minimum" }],
     "new-cap": [2, { "newIsCap": true, "capIsNew": false }],
     "new-parens": 2,
     "no-array-constructor": 2,
